f918a7c4846b8ffe9db0a354fca44f4930e5d953
Caleb Rainey <caleb@crainey.net>
2020-08-08 22:07:51 -0400

    change BOTS env var to work with Heroku

--- app.rb
+++ app.rb
@@ -2,8 +2,16 @@
 post "/" do
   message = params
   message = JSON.parse(request.body.read)
+  # parse the BOTS environment variable
+  bots = []
+  BOTS.split("|").each do |s|
+    strings = s.split(";")
+    bot_id = strings[0].split(":")[1]
+    group_id = strings[2].split(":")[1]
+    bots << { :bot_id => bot_id, :group_id => group_id }
+  end
   # select the appropriate bot for this message
-  bot = BOTS.select{ |b| b[:group_id].to_s == message["group_id"].to_s }.first
+  bot = bots.select{ |b| b[:group_id].to_s == message["group_id"].to_s }.first
   unless bot == nil || bot.empty? # don't run unless we have a bot for this group
     if message["text"][0] == "!" #commands start with "!", but you can define a different starting char as needed
       CommandProcessor.process(message, bot)
