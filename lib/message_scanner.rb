ce7fab6dbd6731a398047fc2796c4bbaee477671
Caleb Rainey <caleb@crainey.net>
2020-08-08 22:36:28 -0400

    spook scanner now works for all users

--- lib/message_scanner.rb
+++ lib/message_scanner.rb
@@ -1,12 +1,10 @@
 class MessageScanner
   def self.scan
-    if message["name"].downcase.include?("stefan") || message["name"].downcase.include?("ze")
-      grinch_keys = ["halloween", "spook", "grinch", "october"]
-      grinch_keys.each do |key|
-        if message["text"].downcase.include?(key)
-          message["text"] = "!stefanchristmas"
-          CommandProcessor.process_command(message, bot_id)
-        end
+    grinch_keys = ["halloween", "spook", "grinch", "october"]
+    grinch_keys.each do |key|
+      if message["text"].downcase.include?(key)
+        message["text"] = "!stefanchristmas"
+        CommandProcessor.process_command(message, bot_id)
       end
     end
     if message["text"].downcase.include?("spoil")
